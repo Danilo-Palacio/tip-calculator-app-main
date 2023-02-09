c8f3d765598ab8d7e7f947aa140a138839672ccf
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2023-02-09 09:59:03 -0300

    corrections

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import BtnSelectTip from './components/BtnSelectTip';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <BtnSelectTip/>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BtnSelectTip/index.js
@@ -0,0 +1,10 @@
+import './BtnSelectTip.css'
+
+const BtnSelectTip = () => {
+    return(
+        <button>5%</button>
+    )
+
+}
+
+export default BtnSelectTip
